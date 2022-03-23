b573b05afdd335ee7778d64bc753e8d65040df41
domashny-diktator <91147437+domashny-diktator@users.noreply.github.com>
2022-03-23 13:30:45 +0300

    Add files via upload

---
+++ lab12/lab12/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.textBox17 = new System.Windows.Forms.TextBox();
+            this.textBox18 = new System.Windows.Forms.TextBox();
+            this.textBox19 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.textBox24 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 362);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(674, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 392);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(630, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(630, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(27, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(630, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(27, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(630, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(27, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(630, 167);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(27, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(630, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(27, 20);
+            this.textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(630, 219);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(27, 20);
+            this.textBox6.TabIndex = 9;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(630, 251);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(27, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(630, 275);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(27, 20);
+            this.textBox8.TabIndex = 11;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(630, 301);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(27, 20);
+            this.textBox9.TabIndex = 12;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(630, 327);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(27, 20);
+            this.textBox10.TabIndex = 13;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(630, 353);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(27, 20);
+            this.textBox11.TabIndex = 14;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(630, 379);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(27, 20);
+            this.textBox12.TabIndex = 15;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(714, 379);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(27, 20);
+            this.textBox13.TabIndex = 27;
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(714, 353);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(27, 20);
+            this.textBox14.TabIndex = 26;
+            // 
+            // textBox15
+            // 
+            this.textBox15.Location = new System.Drawing.Point(714, 327);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(27, 20);
+            this.textBox15.TabIndex = 25;
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(714, 301);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(27, 20);
+            this.textBox16.TabIndex = 24;
+            // 
+            // textBox17
+            // 
+            this.textBox17.Location = new System.Drawing.Point(714, 275);
+            this.textBox17.Name = "textBox17";
+            this.textBox17.Size = new System.Drawing.Size(27, 20);
+            this.textBox17.TabIndex = 23;
+            // 
+            // textBox18
+            // 
+            this.textBox18.Location = new System.Drawing.Point(714, 251);
+            this.textBox18.Name = "textBox18";
+            this.textBox18.Size = new System.Drawing.Size(27, 20);
+            this.textBox18.TabIndex = 22;
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(714, 219);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(27, 20);
+            this.textBox19.TabIndex = 21;
+            // 
+            // textBox20
+            // 
+            this.textBox20.Location = new System.Drawing.Point(714, 193);
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(27, 20);
+            this.textBox20.TabIndex = 20;
+            // 
+            // textBox21
+            // 
+            this.textBox21.Location = new System.Drawing.Point(714, 167);
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(27, 20);
+            this.textBox21.TabIndex = 19;
+            // 
+            // textBox22
+            // 
+            this.textBox22.Location = new System.Drawing.Point(714, 141);
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(27, 20);
+            this.textBox22.TabIndex = 18;
+            // 
+            // textBox23
+            // 
+            this.textBox23.Location = new System.Drawing.Point(714, 115);
+            this.textBox23.Name = "textBox23";
+            this.textBox23.Size = new System.Drawing.Size(27, 20);
+            this.textBox23.TabIndex = 17;
+            // 
+            // textBox24
+            // 
+            this.textBox24.Location = new System.Drawing.Point(714, 89);
+            this.textBox24.Name = "textBox24";
+            this.textBox24.Size = new System.Drawing.Size(27, 20);
+            this.textBox24.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(630, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(716, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(860, 427);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.textBox17);
+            this.Controls.Add(this.textBox18);
+            this.Controls.Add(this.textBox19);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox23);
+            this.Controls.Add(this.textBox24);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.TextBox textBox24;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab12/lab12/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab12
+{
+    public partial class Form1 : Form
+    {
+        const int NMAX = 500;
+        const double BIG = 1.0e30;
+        Graphics dc; Pen p;
+        int n; int[] v; double[] x; double[] y;
+        ComboBox tbComboBox1;
+        // Для динамических TextBox
+        public const int bh = 40; public const int bw = 40;
+        public static int iRows = 12, iColumns = 2;
+        public TextBox[,] tbArray;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            v = new int[NMAX];
+            x = new double[NMAX];
+            y = new double[NMAX];
+            x[0] = 1; x[1] = 6; x[2] = 6; x[3] = 4; x[4] = 4; x[5] = 5; x[6] = 5; x[7] = 2;
+            x[8] = 2; x[9] = 3; x[10] = 3; x[11] = 1;
+            y[0] = 1; y[1] = 1; y[2] = 4; y[3] = 4; y[4] = 3; y[5] = 3; y[6] = 2; y[7] = 2;
+            y[8] = 3; y[9] = 3; y[10] = 4; y[11] = 4;
+            dc = pictureBox1.CreateGraphics();
+            p = new Pen(Brushes.Red, 1);
+            // Создание динамического ComboBox
+            tbComboBox1 = new ComboBox()
+            {
+                Location = new Point(61, 31),
+                Width = 121,
+                Height = 21
+            };
+
+            panel1.Controls.Add(tbComboBox1);
+            for (int i = 0; i < NMAX; i++) { tbComboBox1.Items.Add(i); }
+            tbComboBox1.SelectedItem = 12;
+            // Создание динамических TextBox
+            Create(iRows, iColumns);
+        }
+
+        // Создаёт динамически TextBoxы для ввода координат вершин полигона
+        public void Create(int rows, int columns)
+        {
+            tbArray = new TextBox[rows, columns]; int y1 = 80;
+            for (int i = 0; i < rows; i++)
+            {
+                int x1 = 60; y1 += bh - 10;
+                for (int j = 0; j < columns; j++)
+                {
+                    tbArray[i, j] = new TextBox(); tbArray[i, j].Name = "TextBox" + i + j;
+                    panel1.Controls.Add(tbArray[i, j]);
+                    tbArray[i, j].SetBounds(x1, y1, bw, bh);
+                    x1 += bw;
+
+                }
+                tbArray[i, 0].Text = x[i].ToString("R");
+                tbArray[i, 1].Text = y[i].ToString("R");
+
+            }
+        }
+        /* Метод преобразования вещественной координаты X в целую */
+        private int IX(double x)
+        { double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5; return (int)xx; }
+        /* Метод преобразования вещественной координаты Y в целую */
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5;
+
+            return (int)yy;
+        }
+        /* Своя функция вычечивания линии (экран 10х7 условных единиц) */
+        private void Draw(double x1, double y1, double x2, double y2)
+        {
+            Point point1 = new Point(IX(x1), IY(y1)); Point point2 = new Point(IX(x2), IY(y2));
+            dc.DrawLine(p, point1, point2);
+        }
+
+        private unsafe bool counter_clock(int h, int i, int j, double* pdist)
+        {
+            double xh = x[v[h]], xi = x[v[i]], xj = x[v[j]],
+            yh = y[v[h]], yi = y[v[i]], yj = y[v[j]],
+            x_hi, y_hi, x_hj, y_hj, Determ;
+            x_hi = xi - xh; y_hi = yi - yh; x_hj = xj - xh; y_hj = yj - yh;
+            *pdist = x_hj * x_hj + y_hj * y_hj;
+            Determ = x_hi * y_hj - x_hj * y_hi;
+            return (Determ > 1e-6);
+        }
+        private void draw_polygon()
+        {
+            int i; double xold, yold;
+            xold = x[n - 1]; yold = y[n - 1];
+            for (i = 0; i < n; i++)
+            {
+                Draw(xold, yold, x[i], y[i]); xold = x[i]; yold = y[i];
+            }
+        }
+    }
+}
