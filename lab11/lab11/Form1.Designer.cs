48dd1850cc46ff2884efbb7dc8ad5c6aa39336e7
domashny-diktator <91147437+domashny-diktator@users.noreply.github.com>
2022-03-23 13:29:04 +0300

    Add files via upload

---
+++ lab11/lab11/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.записатьВФайлSCRATCHToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 366);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(486, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(628, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(486, 129);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(628, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(486, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "x1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(625, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "y1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "x2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(625, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "y2";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.записатьВФайлSCRATCHToolStripMenuItem,
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem,
+            this.очиститьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(772, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // записатьВФайлSCRATCHToolStripMenuItem
+            // 
+            this.записатьВФайлSCRATCHToolStripMenuItem.Name = "записатьВФайлSCRATCHToolStripMenuItem";
+            this.записатьВФайлSCRATCHToolStripMenuItem.Size = new System.Drawing.Size(166, 20);
+            this.записатьВФайлSCRATCHToolStripMenuItem.Text = "Записать в файл SCRATCH";
+            this.записатьВФайлSCRATCHToolStripMenuItem.Click += new System.EventHandler(this.записатьВФайлSCRATCHToolStripMenuItem_Click_1);
+            // 
+            // прочитатьИзФайлпаИОтобразитьToolStripMenuItem
+            // 
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem.Name = "прочитатьИзФайлпаИОтобразитьToolStripMenuItem";
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem.Size = new System.Drawing.Size(211, 20);
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem.Text = "прочитать из файлпа и отобразить";
+            this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem.Click += new System.EventHandler(this.прочитатьИзФайлпаИОтобразитьToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.очиститьToolStripMenuItem.Text = "очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click_1);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.выходToolStripMenuItem.Text = "выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 419);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem записатьВФайлSCRATCHToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem прочитатьИзФайлпаИОтобразитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ lab11/lab11/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab11
+{
+    public partial class Form1 : Form
+    {
+        public struct Simple
+        {
+            public double xx; public double yy; public int ii;
+        };
+        FileInfo my_file = new FileInfo("SCRATCH");
+        double x, y, xmin, xmax, ymin, ymax; double X, Y, Xmin, Xmax, Ymin, Ymax;
+        double fx, fy, f, xC, yC, XC, YC, c1, c2;
+        Simple s; Graphics dc; Pen p;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int IX(double x)
+        {
+            double xx = x * (pictureBox1.Size.Width / 10.0) + 0.5; return (int)xx;
+        }
+        private int IY(double y)
+        {
+            double yy = pictureBox1.Size.Height - y * (pictureBox1.Size.Height / 7.0) + 0.5;
+            return (int)yy;
+        }
+        double middle(double x1, double x2)
+        { return (x1 + x2) / 2; }
+        void star(double xc, double yc, double r)
+        {
+            double phi, phi_, r_half, r_double, factor = 0.0174533;
+            double temp_x, temp_y;
+            int i;
+            if (r < 0.1) return;
+            using (BinaryWriter fw = new BinaryWriter(my_file.Open(FileMode.Open,
+            FileAccess.Write, FileShare.Write)))
+            {
+                fw.Seek(0, SeekOrigin.End);
+                s.xx = xc + r; s.yy = yc; s.ii = 0;
+                fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+                for (i = 1; i <= 5; i++)
+                {
+                    phi = i * 72 * factor;/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    phi_ = (i + 1) * 72 * factor;
+                    s.xx = xc; s.yy = yc; s.ii = 1;
+                    fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+
+                    s.xx = xc + r * Math.Cos(phi); s.yy = yc + r * Math.Sin(phi); s.ii = 1;
+                    fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+                    temp_x = middle(xc + r * Math.Cos(phi), xc + r * Math.Cos(phi_));
+                    temp_y = middle(yc + r * Math.Sin(phi), yc + r * Math.Sin(phi_));
+                    temp_x = middle(temp_x, xc);
+                    temp_y = middle(temp_y, yc);
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    temp_x = middle(temp_x, xc);
+                    temp_y = middle(temp_y, yc);
+
+                    temp_x = middle(xc + r * Math.Cos(phi), xc + r * Math.Cos(phi_));
+                    temp_y = middle(yc + r * Math.Sin(phi), yc +r * Math.Sin(phi_));
+                    temp_x = middle(temp_x, xc);
+                    temp_y = middle(temp_y, yc);
+
+                 
+
+                    s.xx = temp_x; s.yy = temp_y; s.ii = 1;
+                    fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+
+                    s.xx = xc + r * Math.Cos(phi_); s.yy = yc + r * Math.Sin(phi_); s.ii = 1;
+                    fw.Write(s.xx); fw.Write(s.yy); fw.Write(s.ii);
+                }
+                fw.Close();
+            }
+            r_half = r * 0.5;
+            r_double = 2 * r;
+            for (i = 0; i < 5; i++)
+            {
+                phi = (36 + i * 72) * factor;
+
+                star(xc + r_double * Math.Cos(phi), yc + r_double * Math.Sin(phi), r_half);
+
+            }
+        }
+       
+       
+        
+       
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            int x2, y2;
+            textBox1.Text = pictureBox1.Location.X.ToString();
+            textBox2.Text = pictureBox1.Location.Y.ToString();
+            x2 = pictureBox1.Location.X + pictureBox1.Size.Width;
+            textBox3.Text = x2.ToString();
+            y2 = pictureBox1.Location.Y + pictureBox1.Size.Height;
+            textBox4.Text = y2.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x1, y1, x2, y2;
+            x1 = System.Convert.ToInt32(textBox1.Text);
+            y1 = System.Convert.ToInt32(textBox2.Text);
+            pictureBox1.Location = new Point(x1, y1);
+            x2 = x1 + System.Convert.ToInt32(textBox3.Text);
+            y2 = y1 + System.Convert.ToInt32(textBox4.Text);
+            pictureBox1.Size = new Size(x2, y2);
+        }
+
+        private void записатьВФайлSCRATCHToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            using (BinaryWriter fw = new BinaryWriter(my_file.Open(FileMode.Create,
+           FileAccess.Write, FileShare.Write)))
+            {
+                fw.Close();
+            }
+            star(0.0, 0.0, 1);
+        }
+
+        private void прочитатьИзФайлпаИОтобразитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dc = pictureBox1.CreateGraphics(); p = new Pen(Brushes.Red, 1);
+            Point X1Y1 = new Point(); Point X2Y2 = new Point();
+            using (BinaryReader fr = new BinaryReader(my_file.Open(FileMode.OpenOrCreate,
+
+            FileAccess.Read, FileShare.Read)))
+            {
+                while (fr.BaseStream.Position < fr.BaseStream.Length)
+                {
+                    s.xx = fr.ReadDouble();
+                    s.yy = fr.ReadDouble();
+                    s.ii = fr.ReadInt32();
+                    x = s.xx; y = s.yy;
+                    if (x < xmin) xmin = x; if (x > xmax) xmax = x;
+                    if (y < ymin) ymin = y; if (y > ymax) ymax = y;
+                }
+                fr.Close();
+            }
+            Xmin = 0; Xmax = 10; Ymin = 0; Ymax = 7;
+            fx = (Xmax - Xmin) / (xmax - xmin);
+
+            fy = (Ymax - Ymin) / (ymax - ymin);
+            f = (fx < fy ? fx : fy);
+            xC = 0.5 * (xmin + xmax);
+            yC = 0.5 * (ymin + ymax);
+            XC = 0.5 * (Xmin + Xmax);
+            YC = 0.5 * (Ymin + Ymax);
+            c1 = XC - f * xC;
+            c2 = YC - f * yC;
+            using (BinaryReader fr = new BinaryReader(my_file.Open(FileMode.OpenOrCreate,
+            FileAccess.Read, FileShare.Read)))
+            {
+                while (fr.BaseStream.Position < fr.BaseStream.Length)
+                {
+                    s.xx = fr.ReadDouble();
+                    s.yy = fr.ReadDouble();
+                    s.ii = fr.ReadInt32();
+                    x = s.xx; y = s.yy;
+
+                    x = s.xx;
+                    y = s.yy;
+                    X = f * x + c1;
+                    Y = f * y + c2;
+                    X2Y2.X = IX(X);
+                    X2Y2.Y = IY(Y);
+                    if (s.ii == 1) { dc.DrawLine(p, X1Y1, X2Y2); X1Y1 = X2Y2; }
+
+                    else { X1Y1 = X2Y2; }
+
+                }
+                fr.Close();
+            }
+        }
+
+        private void очиститьToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            dc.Clear(Color.White);
+        }
+
+        private void выходToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+
+
+    }
+}
